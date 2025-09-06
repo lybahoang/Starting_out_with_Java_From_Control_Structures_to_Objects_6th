b6104ee355ae458e8bc514e91ee1abf117d5014f
lybahoang <157574648+lybahoang@users.noreply.github.com>
2025-09-06 16:55:27 +0700

    Add SalesPrediction Java class skeleton

---
+++ programingChallenges/05 Sales Prediction/SalesPrediction.java
@@ -0,0 +1,9 @@
+public class SalesPrediction 
+{
+    public static void main(String[] args)
+    {
+        // A named constant to the percentage of s
+        final double SALE_PERCENT = 0.62;
+
+    }    
+}
