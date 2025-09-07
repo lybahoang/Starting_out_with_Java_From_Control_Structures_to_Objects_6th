ef311ec9333d6aa39eba7f1976ca5fc23b7da29d
lybahoang <157574648+lybahoang@users.noreply.github.com>
2025-09-07 10:22:26 +0700

    add pc4 chapter 2

---
+++ programingChallenges/04 Star Pattern/StarPattern.java
@@ -0,0 +1,19 @@
+/**
+    This shows my solution to the programming challenge 4 in chapter 2.
+    Book: Starting out with Java: From Control structures to Objects (6th).
+    Date: 06 September 2025.
+ */
+
+public class StarPattern
+{
+    public static void main(String[] args)
+    {
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ***** ");
+        System.out.println("*******");
+        System.out.println(" ***** ");
+        System.out.println("  ***  ");
+        System.out.println("   *   ");
+    }
+}
