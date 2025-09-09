726cf507bb7a05f11231c0aeabdf45ddcd9f9698
lybahoang <157574648+lybahoang@users.noreply.github.com>
2025-09-09 09:41:47 +0700

    Update PenniesForPay.java

--- Chapter 4/Programming Challenges/04 Pennies for Pay/PenniesForPay.java
+++ Chapter 4/Programming Challenges/04 Pennies for Pay/PenniesForPay.java
@@ -9,9 +9,10 @@ public class PenniesForPay
 {
     public static void main(String[] args)
     {
-        int numOfDays;      // The number of worked days.
-        int totalPay = 1;   // An accumulator to hold the total pay.
-        int salaries = 1;   // The salaries of one day.
+        int numOfDays;                      // The number of worked days.
+        double totalPayInPennies = 0;       // An accumulator to hold the total pay in pennies
+        double salariesInPennies;           // To hold the salary each day in pennies
+        final int PENNY_PER_DOLLAR = 100;   // Conversion factor.
 
         // Create a Scanner object and connect it to the System.in object
         // to read input.
@@ -23,21 +24,32 @@ public class PenniesForPay
             System.out.print("How many days have your worked? ");
             numOfDays = keyboard.nextInt();
 
+            // If the number of days is less than 1, then display a message indicating
+            // which will be a valid input.
             if (numOfDays < 1)
             {
-                System.out.println("Error: The number of days should at least 2.");
+                System.out.println("Error: The number of days should at least 1.");
             }
         } while (numOfDays < 1);
 
         // Display the table of salaries during the period of time and
         // also update the accumulator.
-        System.out.println("Days\tSalaries");
+        System.out.println("Days\tSalaries ($)");
         System.out.println("---------------");
         for (int count = 1; count <= numOfDays; count++)
         {
-            salaries *= 2;
-            System.out.println(count + "\t" + salaries);
-            
+            // Calculate the salaries for the corresponding day.
+            salariesInPennies = 1 * Math.pow(2.0, (double)(count - 1));
+
+            // Accumulate the salaries.
+            totalPayInPennies += salariesInPennies;
+
+            // Display the report salaries for each day.
+            System.out.println(count + "\t" + salariesInPennies / PENNY_PER_DOLLAR);
         }
+
+        // Display the total pay.
+        System.out.println("------------------");
+        System.out.println("Total pay: $" + totalPayInPennies / PENNY_PER_DOLLAR);
     }
 }
